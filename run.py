b228335ed867505be9debb980bc79fac13790785
lopqto <morpix@protonmail.com>
2017-07-23 09:54:00 +0430

    add blueprint

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from app.utils import get_config
 from app import create_app
 
+# Blueprints
+from api import urls
 config_name = get_config('APP_SETTINGS')  # config_name = "development"
 app = create_app(config_name)
+app.register_blueprint(urls)
 
 if __name__ == '__main__':
     app.run()
